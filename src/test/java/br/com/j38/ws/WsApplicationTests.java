e6475183e6336b38964b0195920ceb18669ffee9
Michel Fernandes <michel.fernandes@localiza.com>
2024-01-13 20:40:02 -0300

    Create application using spring initializer with WebSocket only

---
+++ src/test/java/br/com/j38/ws/WsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.j38.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
